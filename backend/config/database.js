e4dc60e64d4e2a7f6833b97c02739c94c13ef5cb
ZhonghaoWu <zw252@scarletmail.rutgers.edu>
2023-10-09 02:39:05 -0400

    test

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,4 +1,5 @@
 // backend/config/database.js
+/*
 const config = require('./index');
 
 module.exports = {
@@ -21,3 +22,45 @@ module.exports = {
     }
   }
 };
+*/
+// backend/config/database.js
+const config = require('./index');
+
+module.exports = {
+  development: {
+    storage: config.dbFile,
+    dialect: "sqlite",
+    seederStorage: "sequelize",
+    logQueryParameters: true,
+    typeValidation: true
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    define: {
+      schema: process.env.SCHEMA
+    }
+  }
+};
+
+    /*
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+};
+*/
